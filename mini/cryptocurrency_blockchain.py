a5891c9ba3d34104f4618d33b69f46d55f6872fc
yukirin <standupdown@gmail.com>
2018-01-21 22:18:32 +0900

    Fix typo

--- mini/cryptocurrency_blockchain.py
+++ mini/cryptocurrency_blockchain.py
@@ -8,7 +8,7 @@ num_of_blocks_to_add = 20
 
 for i in range(0, num_of_blocks_to_add):
     blocks_to_add = next_block(previous_block)
-    blockchain .append(blocks_to_add)
+    blockchain.append(blocks_to_add)
     previous_block = blocks_to_add
     print("Block {} has been added to the blockchain!".format(blocks_to_add.index))
     print("Hash: {}\n".format(blocks_to_add.hash))
