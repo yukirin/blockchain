fc36f7abd727170e1f8ac135dcd4411e381d9674
yukirin <standupdown@gmail.com>
2018-01-30 16:38:14 +0900

    Add Blockchain

--- learningblockchain/blockchain.py
+++ learningblockchain/blockchain.py
@@ -1,1 +1,20 @@
-print("blockchain")
+# coding: UTF-8
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        pass
+    
+    def new_transaction(self):
+        pass
+
+    @staticmethod
+    def hash(block):
+        pass
+
+    @property
+    def last_block(self):
+        pass
